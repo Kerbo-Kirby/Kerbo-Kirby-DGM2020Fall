de858281124a0099292e65a371cd539fe40a5257
peyton kerbo <10740520@my.uvu.edu>
2020-09-04 15:53:17 -0600

    run and walk
    managed to make it so when you click mouse 0 you would run. and make you walk when yo clicked mouse 1
    
    the only is the walk function where it doesn't go back to the original speed.

--- Practice Unity/Assets/Scenes/scripts/CharaMove.cs
+++ Practice Unity/Assets/Scenes/scripts/CharaMove.cs
@@ -6,13 +6,14 @@ using UnityEngine.UIElements;
 public class CharaMove : MonoBehaviour
 {
 
-    public float moveSpeed = 5f, gravity = -1.5f, rotateSpeed = 15f, jumpForce = 1f, runFast = 1f;
+    public float moveSpeed = 5f, gravity = -1.5f, rotateSpeed = 15f, jumpForce = 1f, runFast = 1f, canWalk = 2f;
     private Vector3 movement;
     private CharacterController _controller;
 
     private float yVar;
 
-    public int runMax = 2;
+    public int runMax = 2, walkMax = 2;
+    
     
     public int jumpCountMax = 2;
 
@@ -34,25 +35,38 @@ public class CharaMove : MonoBehaviour
 
         var  hInput = Input.GetAxis("Horizontal")*Time.deltaTime*rotateSpeed;
         transform.Rotate( 0,hInput, 0);
-        
-      
 
-        
-      
-        
+
+
+
+
+
         if (Input.GetKeyDown(KeyCode.Mouse0) && runFast < runMax)
-        { 
-            
-            
-            
+        {
+
+
+
             moveSpeed = runMax;
 
             runFast++;
 
         }
-            
-        
-        
+
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+
+        {
+
+            moveSpeed = walkMax;
+
+            moveSpeed--; 
+
+
+
+        }
+
+
         yVar += gravity * Time.deltaTime;
 
         if (_controller.isGrounded && movement.y < 0)
