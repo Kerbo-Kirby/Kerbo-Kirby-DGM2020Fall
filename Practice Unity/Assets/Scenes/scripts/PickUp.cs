ad7ba4db27d57661bf9110180c69f3bfab779bf4
peyton kerbo <10740520@my.uvu.edu>
2020-09-06 21:22:32 -0600

    Self Study Pickup
    trying to recreate a pick up with old notes
    
    will fix bugs or look into other ways I can perhaps do it.

---
+++ Practice Unity/Assets/Scenes/scripts/PickUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class PickUp : MonoBehaviour
+{
+    public int points = 1;
+
+    public int Add;
+    
+    
+    
+    public void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Score.Add(points);
+            
+            Destroy(gameObject);
+        }
+
+
+       
+    }
+}
+        
+        
+    
+
+
+   
+        
+     
---
+++ Practice Unity/Assets/Scenes/scripts/PlayerScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour
+{
+    
+    
+    
+    
+    
+    public static void Add(int Add)
+    {
+        Score += Add;
+        Debug.Log("Game Score: " + Score);
+    }
+}
+    
+
