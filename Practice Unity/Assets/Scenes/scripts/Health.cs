b1bd2dece1d9c11617c0eaecc32c1db4adb21963
peyton kerbo <10740520@my.uvu.edu>
2020-09-09 16:16:43 -0600

    classs and health pick up

---
+++ Practice Unity/Assets/Scenes/scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public float waitTIme = 2f;
+        
+        public FloatData HealthMax, HealthCount, powerCount;
+
+
+        private void Start()
+        {
+
+            HealthCount.value = HealthMax.value;
+        }
+
+        public IEnumerator OnTriggerEnter(Collider other)
+        {
+            HealthCount.value = powerCount.value;
+            
+            yield return new WaitForSeconds(waitTIme);
+            HealthCount.value = HealthMax.value;
+            gameObject.SetActive(false);
+        }
+}
