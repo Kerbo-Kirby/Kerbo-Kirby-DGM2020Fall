26b47129b0c2515e71c38ac019069dea1d203cb1
peyton kerbo <10740520@my.uvu.edu>
2020-09-12 16:32:38 -0600

    health

--- Practice Unity/Assets/Scenes/scripts/Health.cs
+++ Practice Unity/Assets/Scenes/scripts/Health.cs
@@ -2,27 +2,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Health : MonoBehaviour
 {
 
-    public float waitTIme = 2f;
+    public float waitTIme = 2f, amount;
         
-        public IntData HealthMax, HealthCount, powerCount;
+        public IntData HealthMax, HealthCount;
 
-
+        public Transform spawnPoint;
         private void Start()
         {
 
             HealthCount.value = HealthMax.value;
         }
+        
+    
+
 
         public IEnumerator OnTriggerEnter(Collider other)
         {
             
             
+            
+            
+            
+
             yield return new WaitForSeconds(waitTIme);
-            HealthCount.value = HealthMax.value;
+            HealthCount.value -= HealthMax.value;
             gameObject.SetActive(false);
+            
+            
+            
+
+            transform.position = spawnPoint.position;
+            HealthCount = HealthMax;
         }
+        
 }
--- Practice Unity/Assets/Scenes/scripts/SpawnPoint.cs
+++ Practice Unity/Assets/Scenes/scripts/SpawnPoint.cs
@@ -2,28 +2,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class SpawnPoint : MonoBehaviour
 {
 
-    public Vector3Data vData, position;
-    
+    public Vector3Data vData;
+        
+        public Vector3 Position;
+
+
+        public GameObject Player;
+        
+        
     //Set the vData from the position value on start
 
 
     public void Start()
     {
+        Position = GetComponent<Vector3>();
         
+        
+
     }
 
 
-    private void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
-        //set the location data of the player to the current spawnPoint
+        //set the location data of the player to the current spawnPo
+        //
+        Vector3 Player = new Vector3(0,0,0);
 
-       
 
 
     }
