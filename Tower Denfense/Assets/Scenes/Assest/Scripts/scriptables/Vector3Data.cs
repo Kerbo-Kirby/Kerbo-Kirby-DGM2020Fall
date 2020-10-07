7e96f26a8d57c1dba6062fc9bfb5ad406ca37c1f
peyton kerbo <10740520@my.uvu.edu>
2020-10-06 19:31:03 -0600

    It Works!

--- Tower Denfense/Assets/Scenes/Assest/Scripts/scriptables/Vector3Data.cs
+++ Tower Denfense/Assets/Scenes/Assest/Scripts/scriptables/Vector3Data.cs
@@ -8,9 +8,32 @@ public class Vector3Data : ScriptableObject
 {
 
 
+    public Vector3 value;
 
-    public Vector3 vecData  ;
+    public void SetValueFromVector3(Vector3 obj)
+    {
+        value = obj;
+    }
 
+    public void SetValueFromPosition(Transform obj)
+    {
+        value = obj.position;
+    }
+
+    public void SetValueFromRotation(Transform obj)
+    {
+        value = obj.eulerAngles;
+    }
+
+    public void SetFromMousePosition(Camera cam)
+    {
+        if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out var hit, 100))
+        {
+            value = hit.point;
+        }
+
+    }
+    
 
    
 }
