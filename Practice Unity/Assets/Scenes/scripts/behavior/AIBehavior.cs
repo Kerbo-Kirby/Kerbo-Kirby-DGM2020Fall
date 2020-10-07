f20ae7a018032f2c68826af6e436bd48d3ce4818
peyton kerbo <10740520@my.uvu.edu>
2020-10-07 16:50:50 -0600

    class

---
+++ Practice Unity/Assets/Scenes/scripts/behavior/AIBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+public class AIBehavior : MonoBehaviour
+{
+  
+    private UnityEngine.AI.NavMeshAgent agent;
+       
+       
+    public Transform player;
+    
+    
+    private void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+    private void Update()
+    {
+        agent.destination = player.position;
+        print("walking");
+    }
+}
+
