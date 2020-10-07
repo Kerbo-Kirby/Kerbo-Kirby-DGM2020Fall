dc6ad8278e6d4a66f718808bdd3e99aedff3eccc
peyton kerbo <10740520@my.uvu.edu>
2020-10-07 16:52:15 -0600

    turrent and obstatcle
    i manage to kinda make a turret by using the look at and courtine we learned in class. will probably sub it for a lerp because i want to make a moving object that shoots at the player

---
+++ Tower Denfense/Assets/Scenes/Assest/Scripts/scriptables/enemyTurret.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyTurret : MonoBehaviour
+{
+
+
+    public Vector3 aim;
+
+    public Transform obj;
+    public float move;
+
+
+
+    void Start()
+    {
+        aim = GetComponent<Vector3>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.LookAt(obj);
+
+        while (move < 0)
+        {
+            new WaitForSeconds(1);
+            transform.Translate(-0.1f, 0, 0);
+            move++;
+        }
+        
+        
+
+
+    }
+
+}
