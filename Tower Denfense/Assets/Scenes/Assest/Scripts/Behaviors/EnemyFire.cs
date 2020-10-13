6fa2ccd17622c2f7211e215922450f2b181c8619
peyton kerbo <10740520@my.uvu.edu>
2020-10-12 19:24:02 -0600

    messing aorud again

---
+++ Tower Denfense/Assets/Scenes/Assest/Scripts/Behaviors/EnemyFire.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFire : MonoBehaviour
+{
+    public Transform prefab;
+     public float ShootTime;
+    public Vector3Data wap;
+    //public GameObject player;
+
+
+
+    IEnumerator Start()
+
+    {
+
+        /// var newObj = Instantiate(prefab, location , Quaternion.identity);
+
+        ;
+        Instantiate(prefab,transform.position, transform.rotation);
+      //  new WaitForSeconds(ShootTime);
+
+
+       yield break;
+
+    }
+    
+}
--- Tower Denfense/Assets/Scenes/Assest/Scripts/Behaviors/InstatncerBehavior.cs
+++ Tower Denfense/Assets/Scenes/Assest/Scripts/Behaviors/InstatncerBehavior.cs
@@ -13,9 +13,11 @@ public class InstatncerBehavior : MonoBehaviour
 
     public void Instance()
     {
-
         var location = transform.position;
-        var newObj = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+        var newObj = Instantiate(prefab, location, Quaternion.Euler(rotAim.value));
+
+
+        
     }
 }
 
