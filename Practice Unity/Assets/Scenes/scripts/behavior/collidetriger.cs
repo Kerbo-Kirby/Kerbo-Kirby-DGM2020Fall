b902f6618a8d0abd7573b857545074ecdd0185e4
peyton kerbo <10740520@my.uvu.edu>
2020-10-13 19:19:23 -0600

    player with colider

---
+++ Practice Unity/Assets/Scenes/scripts/behavior/collidetriger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class collidetriger : MonoBehaviour
+{
+
+    public GameObject newobj;
+
+    public GameObject obj;
+    
+    public Color big = Color.cyan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    private void OnTriggerStay(Collider other)
+    {
+        obj.SetActive(false);
+
+        
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        obj.SetActive(true);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
