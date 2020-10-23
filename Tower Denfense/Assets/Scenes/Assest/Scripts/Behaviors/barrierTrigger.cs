7759bd2df796295f53dc3ef0d32f791665c3e609
peyton kerbo <10740520@my.uvu.edu>
2020-10-22 22:45:36 -0600

    i made a trigger that summons a create
    i did by re using the instancer code

---
+++ Tower Denfense/Assets/Scenes/Assest/Scripts/Behaviors/barrierTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrierTrigger : MonoBehaviour
+{
+    public Transform obj;
+    public Vector3 speed;
+
+    public Vector3Data loc;
+
+  
+    
+    private void Start()
+    {
+
+
+      
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+       
+        speed = transform.position * Time.deltaTime;
+        Instantiate(obj, loc.value, Quaternion.Euler(loc.value));
+  
+        
+        
+        ///this made it a bomb im keeping this for later
+        //   if (gameObject)
+      //  {
+         //   gameObject.SetActive(false);
+
+         //   speed = transform.position * Time.deltaTime;
+            
+         //   gameObject.SetActive(true);
+
+         return; //new WaitForSeconds(1);
+         //  }
+    }
+
+}
