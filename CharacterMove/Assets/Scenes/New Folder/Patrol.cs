f9339e29af5f9397c561f8629bdbac5e68840961
peyton kerbo <10740520@my.uvu.edu>
2020-11-10 23:15:08 -0700

    messing with patrol for guards
    and terrain

---
+++ CharacterMove/Assets/Scenes/New Folder/Patrol.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Patrol : MonoBehaviour
+{
+
+    private WaitForFixedUpdate wtf = new WaitForFixedUpdate();
+
+    public Transform player;
+    private bool canHurt, canPP;
+
+    private NavMeshAgent secretAgent;
+    
+    public List<Vector3Dataq> ppPoints;
+
+    void Start()
+    {
+
+        secretAgent = GetComponent<NavMeshAgent>();
+
+        StartCoroutine(guardPoint());
+    }
+
+    private int i = 0;
+
+    private IEnumerator guardPoint()
+    {
+        canPP = true;
+
+        while (canPP)
+
+        {
+            yield return wtf;
+            if (secretAgent.pathPending || !(secretAgent.remainingDistance < 0.8)) continue;
+            secretAgent.destination = ppPoints[i].value;
+            i = (i + 1) % ppPoints.Count;
+        }
+    }
+}
