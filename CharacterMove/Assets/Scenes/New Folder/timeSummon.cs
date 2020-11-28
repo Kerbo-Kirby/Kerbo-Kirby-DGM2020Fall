fc0ad3c88288bd3175fb81359326dfb6666a1d62
peyton kerbo <10740520@my.uvu.edu>
2020-11-27 20:39:41 -0700

    clean up and experimentation

---
+++ CharacterMove/Assets/Scenes/New Folder/timeSummon.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+
+public class timeSummon : MonoBehaviour
+{
+
+    public float timeApper;
+
+    public GameObject objectApper;
+
+    public void Start()
+    {
+       objectApper.SetActive(false);
+     
+    }
+
+
+    public void Update()
+    {
+
+
+        objectApper.SetActive(true);
+
+
+        
+        }
+}
